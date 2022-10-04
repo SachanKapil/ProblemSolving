3d02bc776f47cdc280dce0f7a47d3b160fe7eaad
kapil.sachan <kapil.sachan@inmobi.com>
2022-10-04 17:33:18 +0530

    Implementation: Solution of CountDigits Problem

--- src/Mathematics/CountDigits.java
+++ src/Mathematics/CountDigits.java
@@ -1,5 +1,16 @@
+package Mathematics;
+
+import java.util.Scanner;
+
 public class CountDigits {
     public static void main(String[] args) {
-        System.out.println("Kapil");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter number:");
+        int number = sc.nextInt();
+        System.out.println(countDigits(number));
+    }
+
+    private static int countDigits(int number) {
+        return (int) Math.log10(number) + 1;
     }
 }
